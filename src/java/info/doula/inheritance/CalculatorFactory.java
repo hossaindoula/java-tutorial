442fd8938bef86f980f484d595ad990167fd9501
Mohammed Hossain Doula <hossaindoula@gmail.com>
2018-01-01 00:47:57 +0600

    Annotation added
    Comment added

--- src/java/info/doula/inheritance/CalculatorFactory.java
+++ src/java/info/doula/inheritance/CalculatorFactory.java
@@ -1,5 +1,14 @@
 package info.doula.inheritance;
 
+/**
+ * Mohammed Hossain Doula
+ *
+ * @hossaindoula | @itconquest
+ * <p>
+ * http://hossaindoula.com
+ * <p>
+ * https://github.com/hossaindoula
+ */
 public abstract class CalculatorFactory {
 
     public static Calculator getInstance() {
